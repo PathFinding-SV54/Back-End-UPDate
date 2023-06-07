e84068ba0d912c1bacf426594d702ed7a64d15e0
JosR <josueramos17@hotmail.com>
2023-06-07 12:57:34 -0500

    feat: added community domain class and interface

---
+++ Domain/CommunityDomain.cs
@@ -0,0 +1,40 @@
+﻿using Infrastructure;
+using Infrastructure.DataClass;
+using Infrastructure.Model;
+
+namespace Domain;
+
+public class CommunityDomain : ICommunityDomain
+{
+    private readonly ICommunityInfrastructure _communityInfrastructure;
+    
+    public CommunityDomain(ICommunityInfrastructure communityInfrastructure)
+    {
+        _communityInfrastructure = communityInfrastructure;
+    }
+
+    public List<Community> GetAll()
+    {
+        return _communityInfrastructure.GetAll();
+    }
+
+    public Community GetById(int id)
+    {
+        return _communityInfrastructure.GetById(id);
+    }
+
+    public bool Create(CommunityData communityData)
+    {
+        return _communityInfrastructure.Create(communityData);
+    }
+
+    public bool Update(int id, CommunityData communityData)
+    {
+        return _communityInfrastructure.Update(id, communityData);
+    }
+
+    public bool Delete(int id)
+    {
+        return _communityInfrastructure.Delete(id);
+    }
+}
