84e5371d561f3accc894f40c3b33b748560fdd79
JosR <josueramos17@hotmail.com>
2023-06-07 12:30:23 -0500

    feat: added community entity to UpdateDbContext

--- Infrastructure/Context/UpdateDbContext.cs
+++ Infrastructure/Context/UpdateDbContext.cs
@@ -7,7 +7,6 @@ public class UpdateDbContext :DbContext
 {
     public UpdateDbContext()
     {
-        
     }
     
     public UpdateDbContext(DbContextOptions<UpdateDbContext> options) : base(options)
@@ -15,7 +14,7 @@ public class UpdateDbContext :DbContext
     }
     
     public  DbSet<Activity> Activities { get; set; }
-
+    public DbSet<Community> Communities { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -38,5 +37,13 @@ public class UpdateDbContext :DbContext
         builder.Entity<Activity>().Property(c => c.Description).HasMaxLength(240);
         builder.Entity<Activity>().Property(c => c.Address).HasMaxLength(60);
         builder.Entity<Activity>().Property(c => c.Date).HasMaxLength(20);
+
+        builder.Entity<Community>().ToTable("communities");
+        builder.Entity<Community>().HasKey(p => p.Id);
+        builder.Entity<Community>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Community>().Property(c => c.Name).IsRequired().HasMaxLength(30);
+        builder.Entity<Community>().Property(c => c.Description).IsRequired().HasMaxLength(500);
+        builder.Entity<Community>().Property(c => c.CreatedAt).IsRequired().ValueGeneratedOnAdd();
+        builder.Entity<Community>().Property(c => c.IsActive).IsRequired();
     }
 }
