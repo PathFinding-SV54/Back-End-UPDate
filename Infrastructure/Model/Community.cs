3603f55dbb444308bde5b74a83017392e6fbc4d1
JosR <josueramos17@hotmail.com>
2023-06-07 11:34:21 -0500

    feat: added community model class

---
+++ Infrastructure/Model/Community.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Infrastructure.Model;
+
+[Table("communities")]
+public class Community
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    
+    [Required]
+    [MaxLength(30)]
+    public string Name { get; set; }
+    
+    [Required]
+    [MaxLength(500)]
+    public string Description { get; set; }
+    
+    [Required]
+    public DateOnly CreatedAt { get; set; }
+}
