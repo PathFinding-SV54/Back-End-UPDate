2da33a05934c5633601bfc879cc22d442af8993a
JosR <josueramos17@hotmail.com>
2023-06-07 19:32:34 -0500

    feat: added participation class

---
+++ Infrastructure/Model/Participation.cs
@@ -0,0 +1,14 @@
+﻿namespace Infrastructure.Model;
+
+public class Participation
+{
+    public int Id { get; set; }
+    
+    //Relationship with Activity
+    public int ActivityId { get; set; }
+    public Activity Activity { get; set; }
+    
+    //Relationship with GroupMember--
+    //public int GroupMemberId { get; set; }
+    //public GroupMember GroupMember { get; set; }
+}
