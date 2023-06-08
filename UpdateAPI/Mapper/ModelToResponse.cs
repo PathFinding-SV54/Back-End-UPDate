899758f00a5cf33d8a7d54a1545c1d9943e39db4
JosR <josueramos17@hotmail.com>
2023-06-08 15:42:53 -0500

    feat: created Automapper classes

---
+++ UpdateAPI/Mapper/ModelToResponse.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using update.Input;
+using Infrastructure.Model;
+
+namespace update.Mapper;
+
+public class ModelToResponse : Profile
+{
+    public ModelToResponse()
+    {
+        CreateMap<CommunityData, Community>();
+        CreateMap<ActivityData, Activity>();
+        CreateMap<Participation, ParticipationData>();
+    }
+}
