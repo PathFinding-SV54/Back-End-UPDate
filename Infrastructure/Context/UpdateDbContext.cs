25832470431611ea8aaeba1c0a7031a617a5bd16
JosR <josueramos17@hotmail.com>
2023-06-07 20:00:46 -0500

    refactor: added participation entity on UpdateDbContext

--- Infrastructure/Context/UpdateDbContext.cs
+++ Infrastructure/Context/UpdateDbContext.cs
@@ -16,6 +16,7 @@ public class UpdateDbContext :DbContext
     public  DbSet<Activity> Activities { get; set; }
     public DbSet<Community> Communities { get; set; }
 
+    public DbSet<Participation> Participations { get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         if (!optionsBuilder.IsConfigured)
@@ -45,5 +46,15 @@ public class UpdateDbContext :DbContext
         builder.Entity<Community>().Property(c => c.Description).IsRequired().HasMaxLength(500);
         builder.Entity<Community>().Property(c => c.CreatedAt).IsRequired().ValueGeneratedOnAdd();
         builder.Entity<Community>().Property(c => c.IsActive).IsRequired();
+
+        builder.Entity<Participation>().ToTable("participations");
+        builder.Entity<Participation>().HasKey(p => p.Id);
+        builder.Entity<Participation>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
+        //Relationship One to Many with Activity
+        builder.Entity<Participation>()
+            .HasOne<Activity>(s => s.Activity)
+            .WithMany(g => g.Participations)
+            .HasForeignKey(s => s.ActivityId);
+        //Relationship One to Many with GroupMembers--
     }
 }
